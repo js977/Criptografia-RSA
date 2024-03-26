37b31861f8dec87511beb1939f2fd5130ee14a41
GeoTiles <pedro@mat.uc.pt>
2024-03-26 19:29:01 +0000

    Modificações para escrita dos tempos

--- CriptoanaliseRSA_GMP/compara.cpp
+++ CriptoanaliseRSA_GMP/compara.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <fstream>
 
 // Use gmpxx.h para utilizar mpz_class
 #include <gmpxx.h> 
@@ -12,16 +13,55 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  // ciclo que gera ns e os passa 
-  string n = "4509540007616669";
-
-  // fixa tempo
-  string comando = "./metodoFermat " + n + ";";
-  // calcula tempo
+  string temposFactorizacao; // string que vai conter o nome do ficheiro
   
-  system(comando.c_str());
+  if (argc < 2) {
+    cout << "Número de argumentos insuficiente\n\n";
+    cout << "Utilização: ./compara <nome_ficheiro_para_escrita>\n\n";
+    return (1); // código de erro - Número de argumentos insuficiente
+  }
+
+  temposFactorizacao = argv[1];
+
+  //abrir o ficheiro CSV com os tempos de factorização, modo escrita
+  ofstream ficheiro( temposFactorizacao.c_str(),  ofstream::out) ; // out -- escrita
+
+  if(! ficheiro){
+    cout << "Impossivel abrir o ficheiro " << argv[1] << "\n\n";
+    return(2); // Código de erro, não é possível abrir o ficheiro para escrita
+  }
+  
+  // escreve no ficheiro o n ; p ; q
+  int p = 29207, q = 37579, n = 1097569853;
+
+  ficheiro << n << " ; " << p << " ; " << q << " ; "; // com ';' final e sem mudar de linha
+
+  ficheiro.close(); // fecha o ficheiro;
+  
+  /* ciclo que gera ns e os passa ... modificar para ser um ciclo
+   * /
+
+  /*
+   * Agora chama os diversos métodos, os quais vão escrever dados no ficheiro
+   * sem mudar de linha;
+   */
+  // chama o método de Fermat com o 'n' e o nome do ficheiro
+  string comando = "./metodoFermat " + to_string(n) + " " + temposFactorizacao + ";";
+
+  system(comando.c_str()); // o método escreve: t1 ; p ; q ; --- com ';' final e sem mudar de linha
+
+  // o outro métodos (divisão, Euclides, Fermat)
+
+  // vai fechar a linha
+  
+  // re-abrir o ficheiro CSV com os tempos de factorização, modo escrita no fim
+  ficheiro.open( temposFactorizacao.c_str(),  ofstream::app) ; // app --- append
+  ficheiro << endl;
+  ficheiro.close(); // volta a fechar o ficheiro;
+
+  // fim do ciclo 
   
   return 0;
 }
--- CriptoanaliseRSA_GMP/metodoFermat.cpp
+++ CriptoanaliseRSA_GMP/metodoFermat.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <string>
+#include <fstream> // necessário para lidar com os ficheiros
 
 // Biblioteca GMP
 #include <gmp.h>
@@ -15,22 +16,39 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-  if (argc < 2) {
+  string temposFactorizacao; // string que vai conter o nome do ficheiro
+  string sn; // o 'n'
+  
+  if (argc < 3) {
     cout << "número de argumentos insuficiente\n\n";
-    cout << "Usar: ./metodoFermat 4509540007616669\n";
+    cout << "Usar: ./metodoFermat <n> <nome_ficheiro_saida>\n";
     return(1);
   }
 
-  string sn =argv[1];
+  sn = argv[1];
+  temposFactorizacao = argv[2];
 
-  cout << sn;
+  //abrir o ficheiro CSV com os tempos de factorização, modo continuação de escrita (append)
+  ofstream ficheiroFermat(temposFactorizacao.c_str(),  ofstream::app) ; // app -- append
+
+
+  if(! ficheiroFermat){
+    cout << "Impossivel abrir o ficheiro " << argv[2] << "\n\n";
+    return(2); // Código de erro, não é possível abrir o ficheiro para continuação de escrita
+  }
+
+  //  cout << sn << temposFactorizacao << endl;
+
   
-  mpz_class n("4509540007616669");
+  mpz_class n=stoi(sn,nullptr,10);
   mpz_class p, q;
+
+  int tempo = 0; // modificar para os procedimentos correctos quanto ao tempo
   
   metodoFermat(n, p, q);
   
-  cout << "método de Fermat : " << p << "\t " << q;
+  ficheiroFermat << tempo << " ; " << p << " ; " << q << " ; "; // não muda de linha (vai continuar a escrever
+  ficheiroFermat.close(); // fecha o ficheiro;
   
   return 0;
 }
